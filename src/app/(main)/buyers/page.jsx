c7a4522f7465b57ec182e1ebc628aab04a4cd25b
karmakarCoder <dev.rodro@gmail.com>
2026-02-23 18:57:15 +0600

    modification

--- src/app/(main)/buyers/page.jsx
+++ src/app/(main)/buyers/page.jsx
@@ -3,20 +3,15 @@ import BuyersHeader from "@/components/BuyersComponents/BuyersHeader";
 import BuyingProcess from "@/components/BuyersComponents/BuyingProcess";
 import HomeBuyingGuideForm from "@/components/BuyersComponents/HomeBuyingGuideForm";
 import SendMessage from "@/components/shared/SendMessage";
-import { getBuyerBannerData, getBuyerBuyingProcessData } from "@/hooks/cms.api";
+import { getBuyerBuyingProcessData } from "@/hooks/cms.api";
 
 export default async function page() {
-  const bannerData = await getBuyerBannerData();
+  // const bannerData = await getBuyerBannerData();
   const buyingProcces = await getBuyerBuyingProcessData();
-  console.log(buyingProcces);
 
   return (
     <div>
-      <CommonBanner
-        title={bannerData?.data?.title}
-        subtitle={bannerData?.data?.sub_title}
-        banner_img={bannerData?.data?.image}
-      />
+      <CommonBanner title={"HOME BUYERS"} subtitle={"RESOURCES FOR"} />
       <BuyersHeader />
       <BuyingProcess data={buyingProcces?.data} />
       <HomeBuyingGuideForm />
